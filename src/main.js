e5a6b1242bfff25a832eceb5f23c8df67d19c913
Leonardo Xavier <leonardoxw@gmail.com>
2024-03-31 21:02:00 -0300

    requisição para envio de PDF

--- src/main.js
+++ src/main.js
@@ -1,9 +1,46 @@
+let file = "";
+const baseUrl = "https://api.pdf.co/v1";
 const upButton = document.querySelector("#getFileBtn");
+const fileInput = document.querySelector("#getFile");
 const checkButton = document.querySelector("#check");
 checkButton.addEventListener("click", () => {
-    alert("ok");
+    alert('Ok');
 });
 
 upButton.addEventListener("click", () => {
-    document.querySelector("#getFile").click();
+    fileInput.click();
+});
+
+fileInput.addEventListener("change", (e) => {
+    file = e.target.files[0];
+    if (file) {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+              'x-api-key': 'wistykest@yahoo.fr_r8PXNFh28OU2t3ggJ84VgL1RYu7L7W1CJTDKvKgDf34V406xLlUnXZdtMMHy80lP'
+              // Adicione outros cabeçalhos conforme necessário
+            },
+            body: formData
+          };
+
+        fetch(`${baseUrl}/file/upload`, requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao fazer upload do arquivo');
+                }
+                return response.json(); // Se a resposta for JSON
+            })
+            .then(data => {
+                console.log('Arquivo enviado com sucesso:', data);
+                // Faça algo com a resposta do servidor, se necessário
+            })
+            .catch(error => {
+                console.error('Erro:', error);
+            });
+    } else {
+        console.error('Nenhum arquivo selecionado');
+    }
 });
